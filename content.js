f409e07c4f34c5f7f8f63b7eeec31431613534e9
Rivelia <kevisor@gmail.com>
2021-04-17 18:05:12 +0200

    Added Prev/Next buttons

--- content.js
+++ content.js
@@ -1,3 +1,63 @@
+const UA_NAV = [
+	'0',
+	'A',
+	'B',
+	'C',
+	'D',
+	'E',
+	'F',
+	'G',
+	'H',
+	'I',
+	'J',
+	'K',
+	'L',
+	'M',
+	'N',
+	'O',
+	'P',
+	'Q',
+	'R',
+	'S',
+	'T',
+	'U',
+	'V',
+	'W',
+	'X',
+	'Y',
+	'Z',
+];
+
+function getCurrentUAPage() {
+	const reg = /\/([^/])\/index.html/.exec(document.location.pathname);
+	return reg && reg.length > 1 ? reg[1] : UA_NAV[0];
+}
+
+function getNewUAPageURL(newPage) {
+	const reg = /\/([^/])\/index.html/.exec(document.location.pathname);
+	if (reg) {
+		return document.location.href.replace(
+			/\/([^/])(\/index.*)/,
+			`/${newPage}$2`
+		);
+	}
+	return document.location.href.replace(/(\/index.*)/, `/${newPage}$1`);
+}
+
+function getPrevUAPage() {
+	const curPage = getCurrentUAPage();
+
+	let newIndex = UA_NAV.indexOf(curPage) - 1;
+	if (newIndex < 0) newIndex += UA_NAV.length;
+	return UA_NAV[newIndex];
+}
+
+function getNextUAPage() {
+	const curPage = getCurrentUAPage();
+
+	return UA_NAV[(UA_NAV.indexOf(curPage) + 1) % UA_NAV.length];
+}
+
 var style = document.createElement('style');
 style.innerHTML = `.BUA-screenshot {
 		max-height: 150px;
@@ -18,12 +78,50 @@ style.innerHTML = `.BUA-screenshot {
 	div.page.contentpage {
 		max-width: 100% !important;
 	}`;
+
 document.getElementsByTagName('head')[0].appendChild(style);
 
 let parser = new DOMParser();
 
 let table = document.getElementsByTagName('table');
 if (table.length > 0) {
+	// Add previous and next buttons.
+	let prevBtn = document.createElement('button');
+	prevBtn.innerText = '<';
+	prevBtn.style.position = 'fixed';
+	prevBtn.style.top = '50%';
+	prevBtn.style.left = '0';
+	prevBtn.style.marginLeft = '32px';
+	prevBtn.style.fontSize = '64px';
+	prevBtn.style.background = 'transparent';
+	prevBtn.style.border = '0';
+	prevBtn.style.color = '#FFFFFF';
+	prevBtn.style.cursor = 'pointer';
+	prevBtn.onclick = (e) => {
+		e.preventDefault();
+		document.location = getNewUAPageURL(getPrevUAPage());
+	};
+
+	let nextBtn = document.createElement('button');
+	nextBtn.innerText = '>';
+	nextBtn.style.position = 'fixed';
+	nextBtn.style.top = '50%';
+	nextBtn.style.right = '0';
+	nextBtn.style.marginRight = '32px';
+	nextBtn.style.fontSize = '64px';
+	nextBtn.style.background = 'transparent';
+	nextBtn.style.border = '0';
+	nextBtn.style.color = '#FFFFFF';
+	nextBtn.style.cursor = 'pointer';
+	nextBtn.onclick = (e) => {
+		e.preventDefault();
+		document.location = getNewUAPageURL(getNextUAPage());
+	};
+
+	document.body.appendChild(prevBtn);
+	document.body.appendChild(nextBtn);
+
+	// Add images next to each named links.
 	table = table[0];
 
 	let rows = table
